27a7be5790aab40fa07ba08d4b7da039286084e7
Teddy Sacilowski <teddy@Teddys-iMac.fios-router.home>
2019-11-05 10:21:31 -0500

    Added policy to deny specific Azure resources

--- paas-resources-with-sentinel/main.tf
+++ paas-resources-with-sentinel/main.tf
@@ -58,3 +58,38 @@ resource "azurerm_sql_firewall_rule" "example" {
   start_ip_address    = "${var.sql_firewall_rule_start_ip_address}"
   end_ip_address      = "${var.sql_firewall_rule_end_ip_address}"
 }
+
+#-------------------------------------------------------------------------------
+# Create a MariaDB Server & DB to Test Sentinel Deny Services Policy
+#-------------------------------------------------------------------------------
+resource "azurerm_mariadb_server" "example" {
+  name                = "mariadb-svr"
+  location            = "${azurerm_resource_group.example.location}"
+  resource_group_name = "${azurerm_resource_group.example.name}"
+
+  sku {
+    name     = "B_Gen5_2"
+    capacity = 2
+    tier     = "Basic"
+    family   = "Gen5"
+  }
+
+  storage_profile {
+    storage_mb            = 51200
+    backup_retention_days = 7
+    geo_redundant_backup  = "Disabled"
+  }
+
+  administrator_login          = "acctestun"
+  administrator_login_password = "H@Sh1CoR3!"
+  version                      = "10.2"
+  ssl_enforcement              = "Enabled"
+}
+
+resource "azurerm_mariadb_database" "example" {
+  name                = "mariadb_database"
+  resource_group_name = "${azurerm_resource_group.example.name}"
+  server_name         = "${azurerm_mariadb_server.example.name}"
+  charset             = "utf8"
+  collation           = "utf8_general_ci"
+}
--- sentinel-policy-sets/paas-rules/sentinel.hcl
+++ sentinel-policy-sets/paas-rules/sentinel.hcl
@@ -1,3 +1,7 @@
 policy "allow-only-specific-cidr" {
     enforcement_level = "soft-mandatory"
 }
+
+policy "deny-azure-services" {
+    enforcement_level = "soft-mandatory"
+}
